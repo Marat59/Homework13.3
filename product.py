63b158008a43706b2b35b7120836399bb928dc4d
marat almatov <almat59@rambler.ru>
2024-02-25 22:05:26 +0300

    Homework 13.3

---
+++ product.py
@@ -0,0 +1,38 @@
+class Product:
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
+
+
+    @classmethod
+    def new_product(cls, name, description, price, quantity, products):
+        for product in products:
+            if name == product.name:
+                quantity += product.quantity
+                if price < product._price:
+                    price = product._price
+        return cls(name, description, price, quantity)
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        if new_price <= 0:
+            print("Введена некорректная цена.")
+
+
+    def __str__(self):
+        return f'{self.name}, {self.description}, {self._price} руб. Остаток: {self.quantity} шт.'
+
+    def __add__(self, other):
+        result = (self._price * self.quantity) + (other._price * other.quantity)
+        return result
