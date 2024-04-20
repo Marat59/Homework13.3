428d9b7d7ddf5f6349219d710041a2a2674d9756
MARAT <almat59@rambler.ru>
2024-04-20 23:28:45 +0300

    Correction of mistakes 2

--- product.py
+++ product.py
@@ -16,19 +16,20 @@ class Product:
         for product in products:
             if name == product.name:
                 quantity += product.quantity
-                if price < product._price:
-                    price = product._price
+                if price < product.__price:
+                    price = product.__price
         return cls(name, description, price, quantity)
 
     @property
     def price(self):
-        return self._price
+        return self.__price
 
     @price.setter
     def price(self, new_price):
         if new_price <= 0:
             print("Введена некорректная цена.")
-
+        else:
+            self.__price = new_price
 
     def __str__(self):
         return f'{self.name}, {self.description}, {self.price} руб. Остаток: {self.quantity} шт.'
